428ed19f29ef008d3119d65512bf5b8caeb25a22
NataliiaKorotenko <119405955+NataliiaKorotenko@users.noreply.github.com>
2024-07-09 07:43:22 +0200

    fini homework

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,20 @@
+export function fetchPhotos(query) {
+  const searchParams = new URLSearchParams({
+    key: '44727991-bc78e4ccccfc4d25cda89bd33',
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
+  const url = `https://pixabay.com/api/?${searchParams}`;
+
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch(error => console.log('Error fetching photos:', error));
+}
+
